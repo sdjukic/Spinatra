c7d8225bb08c3db3db621f1fe238441431d3b3c8
Slavisa Djukic <ssdjuka@gmail.com>
2015-03-04 22:10:49 -0500

    Initial commit. Barebones gem works.

---
+++ lib/spinatra.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+
+# Sinatra scaffolding gem
+require 'thor'
+require 'fileutils'
+
+
+class Spinatra < Thor 
+  
+
+  desc "new APP_NAME",  "Creates scaffold for modular Sinatra application."
+  def new(name)
+  	files = ["#{name.capitalize}.rb",
+  	         "Gemfile",
+  	         "README.md",
+  	         "config.ru"] 
+    main_file = ["$:.unshift File.expand_path('../lib', __FILE__)\n",
+                       "require 'sinatra/base'\n",
+                       "\nclass #{name.capitalize} < Sinatra:Base\n",
+                       "\nend"]
+    gems = ["source 'https://rubygems.org'\n",
+            "\ngem 'sinatra'",
+            "\ngem 'rubocop'",
+            "\ngem 'puma'\n",
+            "\ngroup :test do",
+            "\n  gem 'rack-test'",
+            "\n  gem 'rspec'",
+            "\nend"]
+    readme = ["## #{name.capitalize}"]
+    config_ru = ["require './#{name.capitalize}'\n",
+                 "\nrun #{name.capitalize}.new"]
+    project_directories = ["./#{name.capitalize}",
+    	                   "./#{name.capitalize}/public", 
+                           "./#{name.capitalize}/public/images",
+                           "./#{name.capitalize}/public/javascripts",
+                           "./#{name.capitalize}/public/stylesheets",
+                           "./#{name.capitalize}/views"] 
+   
+
+    project_directories.each { |dir| FileUtils.mkdir dir }
+
+    #main app file 
+    files.each do |f|
+      source_file = File.new("#{name.capitalize}/#{f}", 'w')
+      case f
+        when "#{name.capitalize}.rb" then
+          main_file.each { |line| source_file.write line }
+        when "config.ru"
+           config_ru.each { |line| source_file.write line}
+        when "Gemfile"
+        	gems.each { |line| source_file.write line}
+        when "README.md"
+        	readme.each { |line| source_file.write line}
+      end
+    end  
+    
+  end
+
+end
+
+Spinatra.start
---
+++ spinatra.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name        = 'spinatra'
+  s.version     = '0.0.0'
+  s.date        = '2015-03-04'
+  s.summary     = 'Scafolding for modular Sinatra applications.'
+  s.description = 'Gem that will create files needed to start modular Sinatra app'
+  s.authors     = ["Slavisa Djukic"]
+  s.email       = 'ssdjuka@gmail.com'
+  s.files       = ["lib/spinatra.rb"]
+  s.licence     = 'MIT'
+end
