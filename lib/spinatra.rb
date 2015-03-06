ed94684aabd71e6232a330ce769dfbb41468377d
Slavisa Djukic <ssdjuka@gmail.com>
2015-03-05 23:53:01 -0500

    Solved #1. Directories and files are created only if they don't exist.

--- lib/spinatra.rb
+++ lib/spinatra.rb
@@ -1,54 +1,34 @@
 # Sinatra scaffolding gem
 require 'thor'
 require 'fileutils'
+require 'yaml'
 
+CONF_FILE = 'config/core.yaml'
 
 class Spinatra < Thor 
   
 
   desc "new APP_NAME",  "Creates scaffold for modular Sinatra application."
   def new(name)
-  	files = ["#{name.capitalize}.rb",
-  	         "Gemfile",
-  	         "README.md",
-  	         "config.ru"] 
-    main_file = ["$:.unshift File.expand_path('../lib', __FILE__)\n",
-                       "require 'sinatra/base'\n",
-                       "\nclass #{name.capitalize} < Sinatra:Base\n",
-                       "\nend"]
-    gems = ["source 'https://rubygems.org'\n",
-            "\ngem 'sinatra'",
-            "\ngem 'rubocop'",
-            "\ngem 'puma'\n",
-            "\ngroup :test do",
-            "\n  gem 'rack-test'",
-            "\n  gem 'rspec'",
-            "\nend"]
-    readme = ["## #{name.capitalize}"]
-    config_ru = ["require './#{name.capitalize}'\n",
-                 "\nrun #{name.capitalize}.new"]
-    project_directories = ["./#{name.capitalize}",
-    	                   "./#{name.capitalize}/public", 
-                           "./#{name.capitalize}/public/images",
-                           "./#{name.capitalize}/public/javascripts",
-                           "./#{name.capitalize}/public/stylesheets",
-                           "./#{name.capitalize}/views"] 
-   
+  	
+   data = YAML.load_file(CONF_FILE)
 
-    project_directories.each { |dir| FileUtils.mkdir dir }
 
-    #main app file 
-    files.each do |f|
-      source_file = File.new("#{name.capitalize}/#{f}", 'w')
+    data["project_directories"].each { |dir| FileUtils.mkdir dir }
+    
+    FileUtils.cd("#{name.capitalize}")
+    
+    data['files'].each do |f|
+      source_file = File.new("#{f}", 'w')
       case f
         when "#{name.capitalize}.rb" then
-          main_file.each { |line| source_file.write line }
+          data['main_file'].each { |line| source_file.write line }
         when "config.ru"
-           config_ru.each { |line| source_file.write line}
+           data['config_ru'].each { |line| source_file.write line}
         when "Gemfile"
-        	gems.each { |line| source_file.write line}
+        	data['gems'].each { |line| source_file.write line}
         when "README.md"
-        	readme.each { |line| source_file.write line}
+        	data['readme'].each { |line| source_file.write line}
       end
     end  
     
