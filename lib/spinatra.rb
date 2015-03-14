82c9de471c1e40c04b8cc9256f615b90fed5af42
Slavisa Djukic <ssdjuka@gmail.com>
2015-03-13 21:09:04 -0400

    Added -v [--verbose] option when creating a new project.

--- lib/spinatra.rb
+++ lib/spinatra.rb
@@ -8,74 +8,83 @@ class Spinatra < Thor
   
 
   desc "new APP_NAME",  "Creates scaffold for modular Sinatra application."
+  method_option :verbose, :aliases => '-v', :desc => "Print directories and files created"
   def new(name)
-  	
-    data = {"project_directories" => ["#{name.capitalize}",
-                  "#{name.capitalize}/public",
-                  "#{name.capitalize}/public/images",
-                  "#{name.capitalize}/public/javascripts",
-                  "#{name.capitalize}/public/stylesheets",
-                  "#{name.capitalize}/views"], 
+
+  	verbose_output = options[:verbose]
+
+    data = {
+      "project_directories" => [
+        "#{name.capitalize}",
+        "#{name.capitalize}/public",
+        "#{name.capitalize}/public/images",
+        "#{name.capitalize}/public/javascripts",
+        "#{name.capitalize}/public/stylesheets",
+        "#{name.capitalize}/views"], 
     
-    "files" =>  ["#{name.capitalize}.rb",
-                 "Gemfile",
-                 "README.md",
-                 "config.ru",
-                 "views/layout.slim"],
+      "files" => [
+        "#{name.capitalize}.rb",
+        "Gemfile",
+        "README.md",
+        "config.ru",
+        "views/layout.slim"],
 
-    "#{name.capitalize}.rb" => ["$:.unshift File.expand_path('../lib', __FILE__)\n",
-                    "\nrequire 'sinatra/base'\n",
-                    "require 'slim'\n",
-                    "\nclass #{name.capitalize} < Sinatra:Base\n",
-                    "\nend"],
+      "#{name.capitalize}.rb" => [
+        "$:.unshift File.expand_path('../lib', __FILE__)\n",
+        "\nrequire 'sinatra/base'\n",
+        "require 'slim'\n",
+        "\nclass #{name.capitalize} < Sinatra:Base\n",
+        "\nend"],
 
-    "Gemfile" => ["source 'https://rubygems.org'\n",
-               "\ngem 'sinatra'",
-               "\ngem 'rubocop'"
-               "\ngem 'slim'",
-               "\ngem 'puma'\n",
-               "\ngroup :test do",
-               "\n  gem 'rack-test'",
-               "\n  gem 'rspec'",
-               "\nend"],
+      "Gemfile" => [
+        "source 'https://rubygems.org'\n",
+        "\ngem 'sinatra'",
+        "\ngem 'rubocop'",
+        "\ngem 'slim'",
+        "\ngem 'puma'\n",
+        "\ngroup :test do",
+        "\n  gem 'rack-test'",
+        "\n  gem 'rspec'",
+        "\nend"],
 
-    "README.md" => ["## #{name.capitalize}"],
+      "README.md" => ["## #{name.capitalize}"],
 
-    "config.ru" => ["require './#{name.capitalize}'\n",
-                    "\nrun #{name.capitalize}.new"],
+      "config.ru" => [
+        "require './#{name.capitalize}'\n",
+        "\nrun #{name.capitalize}.new"],
 
-    "views/layout.slim" => ["doctype html\n",
-                           "html\n",
-                           "\nhead\n",
-                           "    title #{name.capitalize} App\n",
-                           "\nbody",
-                           "\n    == yield"]
-                  }
+      "views/layout.slim" => [
+        "doctype html\n",
+        "html\n",
+        "\nhead\n",
+        "    title #{name.capitalize} App\n",
+        "\nbody",
+        "\n    == yield"]
+    }
 
     #data = YAML.load_file(CONF_FILE)
 
-   
-    data["project_directories"].each do |dir| 
+    data["project_directories"].each do |dir|
       unless File.directory?(dir)
-        puts "Creating directory \e[32m #{dir} \e[0m" if FileUtils.mkdir(dir) 
+        puts "Creating directory \e[32m #{dir} \e[0m" if FileUtils.mkdir(dir) and verbose_output
       else
-        puts "Skipped creating \e[31m #{dir} \e[0m"
+        puts "Skipped creating \e[31m #{dir} \e[0m" if verbose_output
       end
     end
-    
+        
     FileUtils.cd("#{name.capitalize}")
     
     data['files'].each do |f|
       unless File.exist?(f)
         source_file = File.new("#{f}", 'w')
         if source_file
-          puts "Creating file \e[32m #{f} \e[0m"   
+          puts "Creating file \e[32m #{f} \e[0m" if verbose_output   
         end
         if data["#{f}"]
           data["#{f}"].each { |line| source_file.write line }
         end
       else
-        puts "Skipped creating \e[31m #{f} \e[0m"    
+        puts "Skipped creating \e[31m #{f} \e[0m" if verbose_output
       end
     end
   end
